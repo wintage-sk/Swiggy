58f68133a2368a37e358cc750691f5463f4443dc
wintage-sk <137615967+wintage-sk@users.noreply.github.com>
2023-08-18 01:29:07 +0530

    Update useRestaurantMenu.js

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -9,7 +9,7 @@ const useRestaurantMenu= (resID) => {
    
     
   useEffect(() => {
-    fetchMenu("http://192.168.135.142:3000/");
+    fetchMenu();
   },[]);
   const fetchMenu = async () => {
     const data = await fetch(
